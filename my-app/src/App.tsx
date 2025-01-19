de7def96aa10bd46538378b6bea77b962fcdbeb2
Dawn <120154112+zen-da-baker@users.noreply.github.com>
2025-01-18 22:48:12 -0800

    Drag and drop partially working

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 // Import styling
 import './styles/taskbar.css';
 import './styles/startmenu.css';
+import './styles/window.css';
 
 import { Desktop } from './desktop/Desktop.tsx';
 
--- my-app/src/desktop/Desktop.tsx
+++ my-app/src/desktop/Desktop.tsx
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Taskbar } from './Taskbar.tsx';
 import { StartMenu } from './StartMenu.tsx';
 
 export function Desktop() {
+
+    const [click, setClick] = useState(false);
+
+    const [x, setX] = useState(50);
+    const [y, setY] = useState(50);
+
+    function reposition(event) {
+        
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+
+        if (click === true) {
+            setX(mouseX);
+            setY(mouseY);
+       }
+    }
+
+    function handleMouseDown() {
+        console.log("down")
+        setClick(true);
+    }
+
+    function handleMouseUp() {
+        console.log("up")
+        setClick(false);
+    }
+
     return (
         <main>
             <h1>Desktop</h1>
+            
+            <div
+                className="window"
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+                onMouseMove={(event) => reposition(event)}
+                
+                >
+                <p>https://donovanfrazier.com/projects/valorant-personality-quiz/index.html</p>
+            
+                <iframe 
+                    id="window-1"
+                    style={{
+                        left: x,
+                        top: y
+                    }}
+                    
+                    className="window" 
+                    src="https://donovanfrazier.com/projects/valorant-personality-quiz/index.html">
+                    </iframe>
+            </div>
+
+                <p>{x}</p>
+                <p>{y}</p>
 
             <Taskbar />
             <StartMenu />
